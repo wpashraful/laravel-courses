e8eb7a8316358fac05ba67e0aade73a94dec6aa8
Ashraful alam <ashrafulalamsajal@gmail.com>
2023-01-01 20:34:04 +0600

    first commit

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,25 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+// import { defineConfig } from 'vite';
+// import laravel from 'laravel-vite-plugin';
+//
+// export default defineConfig({
+//     plugins: [
+//         laravel({
+//             input: [
+//                 'resources/css/app.css',
+//                 'resources/js/app.js',
+//             ],
+//             refresh: true,
+//         }),
+//     ],
+// });
 
-export default defineConfig({
-    plugins: [
-        laravel({
-            input: [
-                'resources/css/app.css',
-                'resources/js/app.js',
-            ],
-            refresh: true,
-        }),
-    ],
-});
+const mix = require(laravel-mix);
+const {vue} = require(laravel-mix);
+
+mix.js('resources/js/app.js', 'public/js').postCss('resources/css/app.css',
+    'public/css',[
+        require('postcss-import'),
+            require('tailwindcss'),
+            require('autoprefixer') ,
+
+        ]);
